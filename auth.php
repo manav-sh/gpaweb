62567b93f57993846b3930ddd4f665c63c37c06c
Shubham Patel <shubhamashishpatel@gmail.com>
2021-02-26 16:11:32 +0530

    Added Comments

--- auth.php
+++ auth.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 
 try {
+	// Connecting to the database by user ID and password with the database name.
     $conn = new PDO("mysql:host=$servername;dbname=gpa", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -12,6 +13,7 @@ try {
 
         $name = $_POST["username"]; 	
         $password = $_POST["password"]; 
+	 // Mentioning the table name i.e : student_login_credentiala and searching for the username and password.
 	$query= SELECT Std_Er,Std_Pass FROM student_login_credentiala WHERE username = '$name' AND  password = '$password';
         $result1 = mysqli_query($query,$conn);
 
@@ -19,6 +21,7 @@ try {
         { 
             $_SESSION["logged_in"] = true; 
             $_SESSION["naam"] = $name; 
+		// If the login will be successful then the user will be directed to the next page.
             echo"Welcome to the GPA App";
         }
         else
